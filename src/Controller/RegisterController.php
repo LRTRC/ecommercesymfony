55b84ad8b8df30cc81ac597d135abaad27ecb950
BERGER-LEVRAULT\Thomas.LOIR <thomas.l@myneolink.onmicrosoft.com>
2024-09-09 11:44:56 +0200

    validators

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Form\RegisterUserType;
+use App\Entity\User;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $registerForm = $this->createForm(RegisterUserType::class);
+        $user = new User();
+
+        $registerForm = $this->createForm(RegisterUserType::class, $user);
+        $registerForm->handleRequest($request);
+
+        if ($registerForm->isSubmitted() && $registerForm->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+        }
+
         return $this->render('register/index.html.twig', ['registerForm' => $registerForm->createView()]);
     }
 }
--- src/Form/RegisterUserType.php
+++ src/Form/RegisterUserType.php
@@ -3,21 +3,69 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\Length;
 
 class RegisterUserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('firstname')
-            ->add('lastname')
-            ->add('submit', SubmitType::class)
+            ->add('email', EmailType::class, [
+                'label' => 'Adresse email',
+                'attr' => ['placeholder' => 'Indiquez votre adresse email']
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'constraints' => [
+                    new Length([
+                        'min' => 4,
+                        'max' => 30
+                    ])
+                ],
+                'first_options' =>
+                ['label' => 'Votre mot de passe',
+                    'attr' => ['placeholder' => 'Mot de passe'],
+                    'hash_property_path' => 'password'],
+                'second_options' =>
+                ['label' => 'Confirmer le mot de passe',
+                    'attr' => ['placeholder' => 'Confirmez le mot de passe'
+                    ]
+                ],
+                'mapped' => false,
+                'label' => 'Confirmer votre mot de passe'
+            ])
+            ->add(
+                'firstname',
+                TextType::class,
+                ['label' => 'Prénom',
+                    'attr' =>
+                    ['placeholder' => 'Indiquez votre prénom']]
+            )
+            ->add(
+                'lastname',
+                TextType::class,
+                ['label' => 'Nom',
+                    'attr' =>
+                    ['placeholder' => 'Indiquez votre nom']]
+            )
+            ->add(
+                'submit',
+                SubmitType::class,
+                ['label' => 'Valider',
+                    'attr' =>
+                    ['class' => 'btn-success'
+                    ]
+                ]
+            )
         ;
     }
 
@@ -25,6 +73,12 @@ class RegisterUserType extends AbstractType
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'constraints' => [
+                new UniqueEntity([
+                    'entityClass' => User::class,
+                    'fields' => 'email'
+                ])
+            ]
         ]);
     }
 }

c0128eb116f00d220596c1f180f6fb808cd9922d
BERGER-LEVRAULT\Thomas.LOIR <thomas.l@myneolink.onmicrosoft.com>
2024-09-09 13:45:26 +0200

    login logout

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/login', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        
+        
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsernam = $authenticationUtils->getLastUsername();
+        
+        
+        
+        
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
+            'error' => $error,
+            'last_username' => $lastUsernam,
+        ]);
+    }
+
+    #[Route('/deconnexion', name: 'app_logout', methods: ['GET'])]
+    public function logout(): never
+    {
+        
+        throw new \Exception('Vous êtes déconnecté');
+
+    }
+}
